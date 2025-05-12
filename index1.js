8d574699d54134a773ebd91f78304b7721828f06
nagendra <155764520+vuntanagendra@users.noreply.github.com>
2025-05-12 21:27:29 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -159,6 +159,7 @@ app.get('/profile', (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
